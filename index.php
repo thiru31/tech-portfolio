70539b5b79f89f68d13d5a937e567b7193a4c7c5
Thirumalai <thirumalaicse2020@gmail.com>
2024-03-29 14:12:46 +0530

    Add meta tags to index.php

--- index.php
+++ index.php
@@ -2,6 +2,13 @@
 <html lang="en">
   <head>
     <meta charset="utf-8" />
+    <meta name="title" content="Thirumalai Nagalingam Portfolio">
+    <meta name="description" content="Thirumalai Nagalingam - Computer Science Engineer Portfolio: Explore Thirumalai's projects, skills, and expertise in computer science engineering through a dynamic portfolio showcasing innovative solutions and technological prowess.">
+    <meta name="keywords" content="Thirumalai, Nagalingam, Velammal Engineering College, Madhavaram">
+    <meta name="robots" content="index, follow">
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta name="language" content="English">
+    <meta name="author" content="Thirumalai">
     <title>Thirumalai Nagalingam</title>
     <meta name="viewport" content="width=device-width,initial-scale=1.0" />
    <!-- font awesome -->
